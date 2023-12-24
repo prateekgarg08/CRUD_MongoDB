7a0e05841e6f6d2323935bcc4eabcb097aa929fd
Prateek Garg <prateekgarg881@gmail.com>
2023-12-24 18:34:40 +0530

    wait

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  return <TopicsList />;
+  return <div>Setting!! please wait</div>;
+  // return <TopicsList />;
 }
